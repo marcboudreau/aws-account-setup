8eb75ba650f89fb7af2b3676f49adc1395829721
Marc Boudreau <marc.boudreau@qlik.com>
2017-04-10 16:15:00 -0400

    Creating a setup-bucket.sh script that automatically setups the account S3 bucket. (#1)

---
+++ setup-bucket.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# setup-bucket.sh
+#	Ensures that the account S3 bucket is in place and correctly configured.
+#
+# Usage:
+#	setup-bucket.sh
+#
+# Environment Variables:
+#	AWS_ACCESS_KEY_ID		The ID of the Access Key to use to make AWS API calls.
+#	AWS_SECRET_ACCESS_KEY	The secret Access Key to use to make AWS API calls.
+#   BUCKET_NAME				The name of the bucket to setup correctly.
+#
+
+set -eu
+
+bucket_name=${BUCKET_NAME}
+
+# Check if the bucket exists
+if ! aws s3api head-bucket --bucket $bucket_name 2> /dev/null; then
+	# Create the bucket
+	if ! aws s3api create-bucket --acl private --bucket $bucket_name > /dev/null; then
+		exit 1
+	fi
+fi
+
+# Make sure the right ACL is set
+aws s3api put-bucket-acl --bucket $bucket_name --acl private
