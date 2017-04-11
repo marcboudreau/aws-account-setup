8912f7b7e335077bc9fe6a7edceb01813362e666
Marc Boudreau <marc.boudreau@qlik.com>
2017-04-10 21:15:01 -0400

    Moved backend configuration to its own .tf.json file.  Removed the bucket.tf.json file.  Replaced references to bucket.tf.json with terraform/backend.tf.json.

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -1,9 +1,4 @@
 terraform {
-  backend "s3" {
-    bucket = "marcboudreau-${var.bucket_name}"
-    key = "terraform/cloud-account-setup/global.tfstate"
-    region = "us-east-1"
-  }
   required_version = ">= 0.9.0"
 }
 
